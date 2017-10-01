4782f7a55693b48b6a14f493a05a311854b356f8
Timothy Downs <timothydowns@gmail.com>
2017-10-01 21:28:46 +1100

    adding helper funcs

--- src/log.lua
+++ src/log.lua
@@ -1,6 +1,7 @@
-local random = require("resty.random")
 local restystring = require("resty.string")
 local redis = require("resty.redis")
+local random = require("resty.random")
+local cjson = require("cjson")
 
 local function new_id()
   local bytes = random.bytes(8)
@@ -8,23 +9,61 @@ local function new_id()
   return hexbytes
 end
 
-print(new_id())
+local function get_summary(item)
+  local req_details = {}
+  for i = 1, #item, 2 do
+	  req_details[item[i]] = item[i+1]
+  end
+	return req_details
+end
 
 local function scan_logs(red)
   local ptr = "0"
-  while true do
-    local req_block = red:xrange("reqs", ptr, "+", "count", 100)
-    if #req_block == 0 then
-      break
-    end
-    for i = 1, #req_block do
-      ptr = req_block[i][1]
-      local this_req = req_block[i][2].uri
-      print(this_req)
-    end
+	local first_ids = {}
+	local uris = {}
+  local req_block = red:xrange("reqs", ptr, "+", "count", 100)
+  for i = 1, #req_block do
+    ptr = req_block[i][1]
+    local this_req = req_block[i][2]
+		local req_details = get_summary(this_req)
+		local id, uri = req_details.id, req_details.uri
+		if not first_ids[id] then
+		  first_ids[id] = ptr
+			uris[uri] = uris[uri] or {}
+			table.insert(uris[uri], id)
+	  end
   end
+	for k, v in pairs(uris) do
+	  print(k)
+	end
+	for i = 1, #uris["/api"] do
+	  local req = uris["/api"][i]
+		return first_ids[req]
+	end
 end
 
+local function fetch_request (red, stream_id, req_id)
+  local ptr = stream_id
+	local uri, req
+	local res = ""
+  local req_block = red:xrange("reqs", ptr, "+", "count", 4)
+  for i = 1, #req_block do
+    ptr = req_block[i][1]
+    local this_req = req_block[i][2]
+		local item = get_summary(this_req)
+		print(item.id, stream_id)
+    if item.id == req_id then
+		  print(cjson.encode(item))
+		  uri = uri or item.uri
+			req = req or item.req
+			res = res .. item.res
+      if item.done == "true" then
+			  return uri, req, res
+			end
+		end
+  end
+  
+end
 
 local function is_blacklisted(red, uri)
   local blocked_urls = red:lrange("urlblacklist", 0, -1)
@@ -32,7 +71,7 @@ local function is_blacklisted(red, uri)
     for i = 1, #blocked_urls do
       if uri == blocked_urls[i] then
         return true
-      end
+     end
     end
   end
   return false
@@ -46,26 +85,34 @@ local function push_logs(premature, req_id, uri, req_body, res_body, res_done)
     return
   end
   
-  red:xadd("reqs", "maxlen", 9000, "*",
+  local a, b = red:xadd("reqs", "maxlen", 9000, "*",
+	         "id", req_id,
            "uri", uri,
-           "req_body", req_body,
-           "res_body", res_body,
-           "res_done", res_done)
-    
+           "req", req_body,
+           "res", res_body,
+           "done", res_done)
 end
 
-push_logs(false, new_id(), "/api", "reqreq", "resres", true)
+local bleh = [[
+local red = redis:new()
+red:set_timeout(1000) -- 1 sec
+red:connect("127.0.0.1", 6379)
+red:del("reqs")
+local id = new_id()
+push_logs(false, id, "/api", "reqreq", "resres1 ", false)
+push_logs(false, id, "/api", "reqreq", "resres2", true)
+--push_logs(false, new_id(), "/api", "reqreq", "resres", true)
+push_logs(false, new_id(), "/poop", "reqreq", "resres", true)
+local first_id = scan_logs(red)
+print("first_id is ", first_id)
+print(fetch_request(red, first_id, id))
+]]
+local _M = {}
+
 
 -- ngx.timer.at(0, push_logs, ngx.var.uri, ngx.var.request_body, ngx.arg[1], ngx.arg[2])
-local notes = [[
-  on req
-    gen id
-    stream callbacks stitched with id
-  reading from stuff
-    list reqs for url prefix
-  kill a req
-    post to /poison
-      get /api/foo
-  access_by_lua
-    lpop verb url
-]]
+
+_M.push_logs = push_logs
+_M.new_id = new_id
+
+return _M
